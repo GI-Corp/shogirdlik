93d3615c33b2d717cd9a68f7fd1eedb27cf6c82e
Islom Gulomkodirov <gicorp@Isloms-MacBook-Pro.local>
2022-09-10 21:09:03 +0500

    Update site

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 var swiper = new Swiper(".mySwiper", {
+    loop: true,
     pagination: {
         el: '.swiper-pagination',
     },
-
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
